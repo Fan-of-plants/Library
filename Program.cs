a3c43477f92e2c7af210d8137c10897b809605a1
school <school@A45PC11>
2025-03-18 09:48:56 +0200

    added CR from CRUD

--- Program.cs
+++ Program.cs
@@ -11,22 +11,48 @@
 	Корекція: видалення зведень про газети за певний рік
 	Табличний звіт: список боржників книг певного автора*/
 
-            Console.WriteLine("--------- Welcome to the Library ---------");
-			Console.WriteLine("\tMENU:");
-			Console.WriteLine("\t1. Add new item");
-			Console.WriteLine("\t2. Show library");
-			Console.WriteLine("\t3. Delete item");
-			Console.WriteLine("\t4. Sort all items");
-			Console.WriteLine("\t5. Search item");
-			Console.WriteLine("\t6. Show item info");
-			Console.WriteLine("\t7. Category info");
-			Console.WriteLine("\t8. Edit archive");
-			Console.WriteLine("\t9. Show obligors list by author");
+Console.WriteLine("--------- Welcome to the Library ---------");
+Console.WriteLine("\tMENU:");
+Console.WriteLine("\t1. Add new item");
+Console.WriteLine("\t2. Show library");
+Console.WriteLine("\t3. Delete item");
+Console.WriteLine("\t4. Sort all items");
+Console.WriteLine("\t5. Search item");
+Console.WriteLine("\t6. Show item info");
+Console.WriteLine("\t7. Category info");
+Console.WriteLine("\t8. Edit archive");
+Console.WriteLine("\t9. Show obligors list by author");
 
+Source item = new();
+
+while (true)
+{
+	Console.Write("Enter your choice: ");
+	int choice = Convert.ToInt32(Console.ReadLine());
+
+	switch (choice)
+	{
+		case 1:
+			Console.Write("Enter item title: ");
+			item.Name = Console.ReadLine();
+            Console.Write("Enter item type: ");
+            item.Type = Console.ReadLine();
+            Console.Write("Enter writing year: ");
+			item.WritingYear = Convert.ToInt32(Console.ReadLine());
+		break;
+		case 2:
+			Console.WriteLine("------Library--------");
+            Console.WriteLine($"Type: {item.Type}");
+            Console.WriteLine($"Name: {item.Name}");
+			Console.WriteLine($"Writing Year: {item.WritingYear}");
+		break;
+	}
+}
 public class Source
 {
 	public string Name { get; set; }
-	public string WritingYear { get; set; }
+	public int WritingYear { get; set; }
+	public string Type { get; set; }
 }
 public class Book:Source
 {
