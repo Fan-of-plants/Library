95e902a178772c870c78529ad66d714e77a09842
school <school@A45PC11>
2025-04-08 10:10:49 +0300

    new puncts

--- Program.cs
+++ Program.cs
@@ -15,17 +15,24 @@ using System.Text.Json;
 
 Console.WriteLine("--------- Welcome to the Library ---------");
 Console.WriteLine("\tMENU:");
-Console.WriteLine("\t1. Add new item");
-Console.WriteLine("\t2. Show library");
-Console.WriteLine("\t3. Delete item");
-Console.WriteLine("\t4. Sort all items");
-Console.WriteLine("\t5. Search item");
-Console.WriteLine("\t6. Show item info");
-Console.WriteLine("\t7. Category info");
-Console.WriteLine("\t8. Save Library to file");
-Console.WriteLine("\t9. Load Library from file");
+Console.WriteLine("\t0. Exit");
+Console.WriteLine("\t1. Add new book");
+Console.WriteLine("\t2. Add new magazine");
+Console.WriteLine("\t3. Add new newspaper");
+Console.WriteLine("\t4. Show library");
+Console.WriteLine("\t5. Delete item");
+Console.WriteLine("\t6. Sort all items");
+Console.WriteLine("\t7. Search book");
+Console.WriteLine("\t8. Search magazine");
+Console.WriteLine("\t9. Search newspaper");
+Console.WriteLine("\t10. Category info");
+Console.WriteLine("\t11. Save Library to file");
+Console.WriteLine("\t12. Load Library from file");
 
-List<Source> books = new();
+List<Book> books = new();
+List<Magazine> magazines = new();
+List<Newspaper> newspapers = new();
+
 
 while (true)
 {
@@ -34,6 +41,10 @@ while (true)
 
 	switch (choice)
 	{
+        case 0:
+            Environment.Exit(0);
+            break;
+        
 		case 1:
 			var newBook = new Book();
 			
@@ -46,6 +57,7 @@ while (true)
 
 			books.Add(newBook);
 			break;
+		
 		case 2:
 			foreach (var item in books)
 			{
@@ -55,14 +67,59 @@ while (true)
 				Console.WriteLine($"Writing Year: {item.WritingYear}");
 			}
 			break;
-		case 8:
+        
+		case 3:
+            for (int i = 0; i < books.Count; ++i)
+                Console.WriteLine($"[{i}] Product: " + books[i].Name);
+
+            Console.Write("Enter number to delete: ");
+            int numToDelete = Convert.ToInt32(Console.ReadLine());
+
+            if (numToDelete < 0 || numToDelete >= books.Count)
+            {
+                Console.WriteLine("Number out of range!");
+                break;
+            }
+
+            books.RemoveAt(numToDelete);
+            Console.WriteLine("Product deleted successfully!");
+            break;
+
+        case 5:
+            Console.Write("Enter product name to find: ");
+            string nameToFind = Console.ReadLine().Trim();
+
+            var foundItem = books.Find(x => x.Name.Contains(nameToFind));
+
+            if (foundItem == null)
+            {
+                Console.WriteLine("Product not found!");
+                break;
+            }
+
+            foundItem.Show();
+            break;
+
+        case 7:
 			string JsonToSave = JsonSerializer.Serialize(books);
 			Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
 			File.WriteAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/books_db.json", JsonToSave);
-			break; 
-		case 9:
+
+            string JsonToSave2 = JsonSerializer.Serialize(magazines);
+            Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+            File.WriteAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/magazines_db.json", JsonToSave);
+
+            string JsonToSave3 = JsonSerializer.Serialize(newspapers);
+            Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+            File.WriteAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/newspapers_db.json", JsonToSave);
+            break; 
+		
+		case 8:
 			string jsonToLoad = File.ReadAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/books_db.json");
-			books = JsonSerializer.Deserialize<List<Source>>(jsonToLoad);
+			books = JsonSerializer.Deserialize<List<Book>>(jsonToLoad);
+
+            string jsonToLoad2 = File.ReadAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/magazines_db.json");
+            magazines = JsonSerializer.Deserialize<List<Magazine>>(jsonToLoad2);
             break;
 	}
 }
@@ -71,20 +128,46 @@ public class Source
 	public string Name { get; set; }
 	public int WritingYear { get; set; }
 	public string Type { get; set; }
+   
 }
 public class Book:Source
 {
 	public string Author { get; set; }
 	public string Genre { get; set; }
 	public string Description { get; set; }
+    public void Show()
+    {
+        Console.WriteLine("------- Product ---------");
+        Console.WriteLine($"Name: {this.Name}");
+        Console.WriteLine($"Category: {this.Type}");
+        Console.WriteLine($"Writing Year: {this.WritingYear}");
+        Console.WriteLine($"Author: {this.Author}");
+        Console.WriteLine($"Genre: {this.Genre}");
+    }
 }
 
 public class Magazine : Source
 {
 	public int Id { get; set; }
+    public void Show()
+    {
+        Console.WriteLine("------- Product ---------");
+        Console.WriteLine($"Name: {this.Name}");
+        Console.WriteLine($"Category: {this.Type}");
+        Console.WriteLine($"Writing Year: {this.WritingYear}");
+        Console.WriteLine($"Id: {this.Id}");
+    }
 }
 
 public class Newspaper : Source
 {
 	public string Theme { get; set; }
+    public void Show()
+    {
+        Console.WriteLine("------- Product ---------");
+        Console.WriteLine($"Name: {this.Name}");
+        Console.WriteLine($"Category: {this.Type}");
+        Console.WriteLine($"Writing Year: {this.WritingYear}");
+        Console.WriteLine($"Theme: {this.Theme}");
+    }
 }
